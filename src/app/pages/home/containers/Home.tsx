d079453d27cf6b9d3f36355909cff7f86e208a3c
st-manhle <manh.le@supremetech.vn>
2021-06-23 11:15:40 +0700

    refactor convention

--- src/app/pages/home/containers/Home.tsx
+++ src/app/pages/home/containers/Home.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const Home = () => {
-  const changeTheme = (theme: string) => {
-    localStorage.setItem('theme', theme);
-    document.documentElement.className = '';
-    document.documentElement.classList.add(`theme-${theme}`);
-    console.log('Theme updated...');
-}
+const THEMES = [
+  'light',
+  'dark'
+];
 
-  const themes = [
-    'light',
-    'dark'
-  ];
+const Home = () => {
 
   React.useEffect(() => {
     changeTheme(localStorage.getItem('theme'));
   }, []);
 
+  const changeTheme = (theme: string) => {
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = '';
+    document.documentElement.classList.add(`theme-${theme}`);
+    console.log('Theme updated...');
+  }
+
   return (
     <select defaultValue={localStorage.getItem('theme')} onChange={(e) => changeTheme(e.target.value)}>
-      {themes.map((theme, index) =>
+      {THEMES.map((theme, index) =>
         <option value={theme} key={index}>{theme}</option>
       )}
     </select>
